f864838e44790e5315a72d9b6add23672c3ea5a6
Jacob Choi <junhyungch@gmail.com>
2023-07-27 23:13:20 -0400

    reduced canva / added routing with room id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,41 +4,27 @@ import { FC, useState } from 'react'
 import { useDraw } from '../../hooks/useDraw';
 import { start } from 'repl';
 import { ChromePicker } from 'react-color'
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 interface PageProps {}
 
 const page: FC<PageProps> = ({}) => {
-  const  [color, setColor] = useState<string>('#000')
-  const { canvasRef, onMouseDown, clear } = useDraw(drawLine)
 
-  function drawLine({prevPoint, currentPoint, ctx}: Draw) {
-    const {x: currX, y: currY } = currentPoint
-    const lineWidth = 5
+  const generateRandomId = () => {
+    return Math.floor(Math.random() * 9000) + 1000;
+  };
 
-    let startPoint = prevPoint ?? currentPoint
-    ctx.beginPath()
-    ctx.lineWidth = lineWidth
-    ctx.strokeStyle = color
-    ctx.moveTo(startPoint.x, startPoint.y)
-    ctx.lineTo(currX, currY)
-    ctx.stroke()
-
-    ctx.fillStyle = color
-    ctx.beginPath()
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
-    ctx.fill()
-  }
-
+  const randomId = generateRandomId();
+  
   return (
     <div className='w-screen h-screen bg-white flex justify-center items-center'>
-    <div className='flex flex-col gap-10 pr-10'>
-      <ChromePicker color={color} onChange={(e) => setColor(e.hex)}/>
-      <button type='button' className='p-2 rounded-md border border-black' onClick={clear}>Clear Canvas</button>
+      <div className='flex flex-col gap-10 pr-10'>
+        <a href={`/rooms/${randomId}`}>
+          <button>to random room</button>
+        </a>
+      </div>
     </div>
-    <canvas onMouseDown={onMouseDown} ref={canvasRef} width={750} height={750} className='border border-black rounded-md'/>
-  </div>
   );
-  
+    
 }
 
 export default page
