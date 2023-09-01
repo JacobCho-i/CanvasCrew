5085e5164455a3b61674342fe38af856fe90e751
Jacob Choi <junhyungch@gmail.com>
2023-08-31 22:49:23 -0400

    fix so that AWS RDS PostgreSQL is now connected to this app

--- src/app/database/db.ts
+++ src/app/database/db.ts
@@ -5,7 +5,15 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PW,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
